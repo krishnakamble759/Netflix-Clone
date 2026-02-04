46521bd158addc51753c3fe7a1eca1cde2c01439
krishnakamble759 <krishnakamble759@gmail.com>
2026-02-04 23:15:03 +0530

    Configure GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
     root: './',
-    base: './',
+    base: '/Netflix-Clone/',
     build: {
         outDir: 'dist',
         rollupOptions: {
